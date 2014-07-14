753eff358672dec7e9bf99ccc37ef88f1612c586
Christopher Mehmed <chrismehmed@gmail.com>
2014-07-14 19:25:21 -0400

    partials from the controller page

--- routes_ex.1/app/controllers/home_controller.rb
+++ routes_ex.1/app/controllers/home_controller.rb
@@ -3,12 +3,11 @@ class HomeController < ApplicationController
 
 
 	def index
-		def method_message
-			
-			title: "This is the METHOD message.", 
-			body: "The text is a method from the home controller .rb file."
-		end
-	
+		@method_message = [
+
+			{title: "This is the METHOD message."},
+			{body: "The text is a method from the home controller .rb file."}
+		]
 	end
 
 	def profile
